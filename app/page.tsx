9235786be5039f5225e2ee39369aee952afdae16
alexefthymioutes <alexefthymiou@gmail.com>
2024-10-14 15:11:02 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import caseStudy from "@/resources/caseStudy.json"
+import caseStudy from "../resources/caseStudy.json"
 
 export default function Home() {
   return (
