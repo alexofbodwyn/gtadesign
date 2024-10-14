789fb93e05d3669ab97248b031351c513a32a342
alexefthymioutes <alexefthymiou@gmail.com>
2024-10-14 22:37:20 +0100

    new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
-import caseStudy from "../resources/caseStudy.json"
+import caseStudy from "../resources/case-study.json"
+import homePageContent from "../resources/home-page.json"
+import Icon, { IconProps } from "../components/icon";
+
 
 export default function Home() {
   return (
-    <main className="flex w-full justify-center pt-[50px]">
-      {caseStudy.map((study, index) => (
-        <div key={`caseStudy-map-${index}`}>
-          <h1>{study.title}</h1>
-          <p>{study.body}</p>
-          <Image alt={study.title} src={study.image} width={300} height={300} />
-          <Link href={`case-studies/${study.url}`}>Link</Link>
+    <main className="flex flex-col w-full justify-center items-center pt-14">
+      <div className="max-w-[600px] w-full mx-auto text-center">
+        <h1 className="text-4xl">{homePageContent.pageTitle}</h1>
+        <p className="mt-10">{homePageContent.pageIntro}</p>
+      </div>
+      <div className="max-w-[1200px] w-full mt-10 p-3 bg-white border border-slate-300 rounded-lg drop-shadow-[0_1px_1px_rgba(0,0,0,0.1)]">
+        <Image alt="GTA Designs" src={homePageContent.heroImage} width={1744} height={800} className="rounded-lg border border-slate-200" />
+      </div>
+      <div className="bg-slate-700 my-12 p-12 w-full flex flex-col items-center text-center">
+        <div className="max-w-[800px]">
+          <h2 className="text-3xl text-slate-200 px-24">{homePageContent.panelTitle}</h2>
+          <p className="text-slate-200 mt-10 px-24">{homePageContent.panelText}</p>
+
+          <div className="flex gap-4 mt-10 text-left">
+            {homePageContent.infoBox.map((box, index) => (
+              <div key={`infoBox-map-${index}`} className="flex flex-col border border-slate-600 p-4 bg-gradient-to-br from-slate-600 to-slate-700 rounded-lg w-1/3 drop-shadow h-32">
+                <div className="icon-chart-no-axes-combined"></div>
+                <div className="flex items-center">
+                  <Icon name={box.icon as IconProps["name"]} color="#bbf7d0" className="w-8 h-8" />
+                  <h3 className="text-2xl text-green-200 font-bold ml-2">{box.tite}</h3>
+                </div>
+                <p className="mt-auto text-slate-300">{box.tite}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </main>
+      </div>
+      <div className="flex w-full justify-center pt-[50px]">
+        {caseStudy.map((study, index) => (
+          <div key={`caseStudy-map-${index}`}>
+            <h3>{study.title}</h3>
+            <p>{study.body}</p>
+            <Image alt={study.title} src={study.image} width={300} height={300} />
+            <Link href={`case-studies/${study.url}`}>Link</Link>
+          </div>
+        ))}
+      </div>
+    </main >
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,15 @@ import { Origami } from "lucide-react"
 
 export default function Header() {
   return (
-    <div className="bg-slate-100 border border-b-slate-500 flex items-center w-full p-4 h-[70px]">
+    <div className="bg-slate-100 border border-b-slate-300 flex items-center w-full p-4 h-[70px] drop-shadow-[0_1px_1px_rgba(0,0,0,0.05)]">
       <div className="flex items-center relative w-full">
         <div className="flex items-center absolute left-0 top-[-3px]">
           <Origami size={32} />
           <span className="ml-2"><span className="text-slate-500 font-bold">GTA</span>DESIGN</span>
         </div>
         <nav className="flex items-center justify-center mx-auto">
-          <Link href="/home" className="mx-3">home</Link>
-          <Link href="/home" className="mx-3">case studies</Link>
+          <Link href="/home" className="mx-3">Home</Link>
+          <Link href="/home" className="mx-3">Case studies</Link>
         </nav>
       </div>
     </div>
